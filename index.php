d080c712c12e41de6ec4704c52ac1efd6ab1e250
MaxTheMonster <maxungless@gmail.com>
2015-07-27 15:46:13 +0100

    Styles and redo
    Redo of main page
    styles adjusted

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
 <?php
-include 'nav.php';
+// include 'nav.php';
 ?>
 
 <html>
 <head>
-  <title>Snapimal - Gotta Snap 'em all</title>
-  <link rel="stylesheet" href="style.css">
+  <title>Snapimal  Gotta Snap 'em all</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
   <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 </head>
 <body>
-  <div class="container">
-    <img src="logo.svg" alt="" />
-  <h1>Gotta Snap 'em all</h1>
+  <div class="box">
+    <!-- <input id="plus" name="plus" />
 
-  <button type="button" class="btn btn-primary">Start</button>
-</div>
-<div class="map">
-  <iframe
-  width="600"
-  height="450"
-  frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBVUjFISeGYXrlqymWN9d3tSmlC1OpKtA0
-    &q=Space+Needle,Seattle+WA" allowfullscreen>
-</iframe>
-</div>
+    <input id="search" name="search" /> -->
+
+  </div>
 </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,5 +1,8 @@
 <nav>
-  <span class="nav-item"><a href="#">Birds</a></span>
+  <link rel="icon"
+        type="image/ico" 
+        href="favicon.ico">
+        <span class="nav-item"><a href="#">Birds</a></span>
   <span class="nav-item"><a href="#"></a></span>
   <span class="nav-item"><a href="#"></a></span>
   <span class="nav-item"><a href="#"></a></span>
