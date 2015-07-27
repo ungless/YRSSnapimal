47a8cdeeeeb48f671466e33e9571d72e22259ac1
MaxTheMonster <maxungless@gmail.com>
2015-07-27 11:43:54 +0100

    SQL Dump
    Dumped SQL onto repo

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <html>
 <head>
   <title>Snapimal - Gotta Snap 'em all</title>
+  <link rel="stylesheet" href="styles/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
 </head>
 <body>
   <h1>Gotta Snap 'em all</h1>
