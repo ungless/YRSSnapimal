d7d6f80817131e9bc3ff712d2bc5e7122be9e15f
MaxTheMonster <maxungless@gmail.com>
2015-07-27 13:50:04 +0100

    Updated nav
    Added nav.php
    Changed styles and index
    Changed SQL dump

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
+<?php
+include 'nav.php';
+?>
+
 <html>
 <head>
   <title>Snapimal - Gotta Snap 'em all</title>
-  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="style.css">
   <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-
 </head>
 <body>
+  <div class="container">
   <h1>Gotta Snap 'em all</h1>
+
+  <button type="button" class="btn btn-primary">Start</button>
+</div>
+<div class="map">
+  <iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBVUjFISeGYXrlqymWN9d3tSmlC1OpKtA0
+    &q=Space+Needle,Seattle+WA" allowfullscreen>
+</iframe>
+</div>
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,7 @@
+<nav>
+  <span class="nav-item"><a href="#">Birds</a></span>
+  <span class="nav-item"><a href="#"></a></span>
+  <span class="nav-item"><a href="#"></a></span>
+  <span class="nav-item"><a href="#"></a></span>
+  <a class="button" href="#">Submit a new Snapimal</a>
+</nav>
