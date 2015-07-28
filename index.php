21776faa20d9b27790104d93ef6d2dc7e4007c3d
Max Ungless <maxungless@gmail.com>
2015-07-28 20:17:21 +0100

    Optimised folders

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 <html>
 <head>
   <title>Snapimal - Gotta Snap 'em all</title>
-  <link rel="stylesheet" href="style.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-  <link rel="icon" type="image/ico" href="favicon.ico">
+  <link rel="icon" type="image/ico" href="img/favicon.ico">
 </head>
 <body>
   <div class="box">
     <input type="text"  align="center" class="search-bar" placeholder=""></input>
-    <img src="search.svg" alt="search" class="search-ico"/>
-    <img src="plus.svg" alt="Submit new find" class="sub-ico"/>
+    <img src="img/search.svg" alt="search" class="search-ico"/>
+    <img src="img/plus.svg" alt="Submit new find" class="sub-ico"/>
   </div>
   <script src="js/jquery.js"></script>
 </body>
