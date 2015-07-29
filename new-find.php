ecd33b563cb107ffb68d4df5ba6b9a94dffa2645
MaxTheMonster <maxungless@gmail.com>
2015-07-29 15:47:41 +0100

    Changed some support

--- new-find.php
+++ new-find.php
@@ -2,8 +2,6 @@
 // includes the database settings
 include ('db.php');
 
-
-
 // Make sure the project name and description exists.
 if ( isset($_POST['name']) && isset($_POST['description'])) {
 
@@ -15,34 +13,28 @@ var_dump( $_FILES );
 print_r( $_FILES );
 
 $uploads_dir = 'C:/xampp/htdocs/YRSSnapimal/image_uploads';
-foreach ($_FILES["image"]["error"] as $key => $error) {
-    if ($error == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES["image"]["tmp_name"][$key];
-        $name = $_FILES["image"]["name"][$key];
-        move_uploaded_file($tmp_name, "$uploads_dir/$name");
-    }
+$error = $_FILES["image"]["error"];
+
+if ($error == UPLOAD_ERR_OK) {
+    $tmp_name = $_FILES["image"]["tmp_name"];
+    $name = $_FILES["image"]["name"];
+    move_uploaded_file($tmp_name, "$uploads_dir/$name");
 }
 // Building query
-    $sql="INSERT INTO $tbl_name (name, description, image, location) VALUES('" . $_POST['name']  . "','" . $_POST['description']  . "','" . $_POST['image']  . "','" . $_POST['location']  . "')";
+    $sql = "INSERT INTO $tbl_name (name, description, image, location) VALUES('"
+    . $_POST['name']  . "','"
+    . $_POST['description']  . "','"
+    . $name  . "','"
+    . $_POST['location']  .
+    "')";
 
-     mysql_query($sql);
-
-     print mysql_error();
+    if (!mysql_query($sql)) {
+      echo "Something went wrong! :(";
     }
+     print mysql_error();
+}
 
-?>
-
-<html>
-
-    <?php
-    print  $_POST['name'];
-    print $_FILES['image'];
-    print '<div><span style="color: #a89898; ">Description &#187; </span><span style="color: #0093ff; font-weight: bold;">' . $_POST['description'] . '</span></div>';
-    print '<div><span style="color: #a89898; ">Owners &#187; </span><span style="color: #0093ff; font-weight: bold;">' . $_POST['location'] . '</div>';
-    print '<div><span style="color: #a89898; ">Language &#187; </span><span style="color: #0093ff; font-weight: bold;">' . $_POST['image'] . '</div>';
-    print '</div></center>';
+    // header('Location: http://max.dev/YRSSnapimal/index.html');
+    print 'image: <img src="image_uploads/'. $name . '"height="100px" width="100px"></br>';
 
     ?>
-    </body>
-    </font>
-</html>
--- radnom-form.php
+++ radnom-form.php
@@ -1,6 +1,6 @@
-<form name="new-find" action="new-find.php" method="post">
+<form name="new-find" action="new-find.php" method="post" enctype="multipart/form-data">
   <input type="text" name="name" value="" placeholder="Name of animal">
   <input type="text" name="description" value="" placeholder="Description" rows="10">
-  <input type="file" name="image" accept=".jpeg,.jpg,.png" />
+  <input type="file" name="image" accept=".jpeg,.jpg,.png,.gif"/>
   <input type="submit" name="submit" value="Submit Find">
 </form>
